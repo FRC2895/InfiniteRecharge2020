4f752cfe178cba97a4525608d0d05c24eb307c3b
Joe Pokorny <team2895@outlook.com>
2020-02-08 13:52:32 -0500

    Mapped Controller Buttons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -18,38 +18,43 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * Runs the motors with arcade steering.
  */
 public class Robot extends TimedRobot {
-  private final int BUTTON_A = 0;
-  private final int BUTTON_B = 1;
-  private final int BUTTON_X = 2;
-  private final int BUTTON_Y = 3;
-  private final int BUTTON_LT = 4;
-  private final int BUTTON_RT = 5;
-  private final int BUTTON_BACK = 6;
-  private final int BUTTON_START = 7;
-  private final int BUTTON_L_ANALOG = 8;
-  private final int BUTTON_R_ANALOG = 9;
+  private final int BUTTON_A = 1;
+  private final int BUTTON_B = 2;
+  private final int BUTTON_X = 3;
+  private final int BUTTON_Y = 4;
+  private final int BUTTON_LT = 5;
+  private final int BUTTON_RT = 6;
+  private final int BUTTON_BACK = 7;
+  private final int BUTTON_START = 8;
+  private final int BUTTON_L_ANALOG = 9;
+  private final int BUTTON_R_ANALOG = 10;
 
   private final Spark m_leftMotor = new Spark(0);
   private final Spark m_rightMotor = new Spark(1);
   private final Spark m_intake = new Spark(2);
   private final Spark m_shooter_left = new Spark(3);
   private final Spark m_shooter_right = new Spark(4);
+  private final Spark m_colorWheelMotor = new Spark(5);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final DifferentialDrive m_shooter = new DifferentialDrive(m_shooter_right, m_shooter_left);
-  private final Joystick m_stick = new Joystick(0);
+  private final Joystick m_cStick = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    System.out.println("Robot Init");
+    super.robotInit();
+  }
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    m_robotDrive.arcadeDrive(m_cStick.getY(), m_cStick.getX());
+    m_shooter.arcadeDrive(0, 0);
+  }
 
-    if(m_stick.getRawButton(BUTTON_X)) {
-      double speed = 0.75;
-      m_shooter.arcadeDrive(speed, 0);
-    } else {
-      m_shooter.arcadeDrive(0, 0);
-    }
+  @Override
+      public void autonomousPeriodic() {
   }
 }
